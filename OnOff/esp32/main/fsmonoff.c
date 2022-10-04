fbafc4a96c119fbce6ec57f1e6c1a9ff7e173bf3
vinlred <78894721+vinlred@users.noreply.github.com>
2022-10-04 11:58:10 +0700

    Add files via upload

---
+++ OnOff/esp32/main/fsmonoff.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fsmonoff.h"
+
+void fsm(int input,int *prev_state,int *state,int *change_count,int *output){
+	switch(*state)
+	{
+	case STATE_ON:
+	{
+		if(input == 0){
+		}
+		else if(input == 1){
+			*state = CHANGE;
+			*prev_state = STATE_ON;
+			*change_count = 0;
+		}
+		break;
+	}
+	case CHANGE:
+	{
+		if(*change_count <= 400){ // Jumlah diatur sesuai frekuensi untuk mencapai 1000 ms
+			if(input == 1){
+				*change_count += 1;
+			}
+			else{
+				*state = STATE_ON;
+			}
+		}
+		else{
+			*state = STATE_TRANS;
+			*change_count = 0;
+			*output = 0;
+		}
+		break;
+	}
+	case STATE_TRANS:
+	{
+		if(input == 0){
+			if(*prev_state == STATE_ON){
+				*state = STATE_OFF;
+			}
+			else{
+				*state = STATE_ON;
+			}
+		}
+		break;
+	}
+	case STATE_OFF:
+	{
+		if(input == 1){
+			*state = STATE_TRANS;
+			*prev_state = STATE_OFF;
+			*output = 1;
+		}
+		break;
+	}
+	default:
+	{
+		break;
+	}
+	}
+}
+
+
+
+
+
---
+++ OnOff/esp32/main/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "fsmonoff.h"
+#include "fsmonoff.c"
+
+#define GPIO_OUTPUT 4
+#define GPIO_OUTPUT_PIN_SEL (1ULL<<GPIO_OUTPUT)
+#define GPIO_INPUT_PB 5
+#define GPIO_INPUT_PIN_SEL (1ULL<<GPIO_INPUT_PB)
+
+void app_main(){
+	gpio_config_t io_conf;
+	io_conf.intr_type = 0;
+	io_conf.mode = GPIO_MODE_OUTPUT;
+	io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+	io_conf.pull_down_en = 0;
+	io_conf.pull_up_en = 0;
+	gpio_config(&io_conf);
+	io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+	io_conf.mode = GPIO_MODE_INPUT; // mode input
+	io_conf.pull_up_en = 1; // menggunakan pull up
+	gpio_config(&io_conf);
+	
+	int input = 0;
+	int prev_state = 0;
+	int state = 0;
+	int output = 0;
+	int change_count = 0;
+	
+	while(1){
+		input = !(gpio_get_level(GPIO_INPUT_PB));
+		fsm(input, &prev_state, &state, &change_count,&output);
+		printf("Input: %i,Output: %i, State: %i\n", input,output,state);
+		gpio_set_level(GPIO_OUTPUT, output);
+	}
+}
