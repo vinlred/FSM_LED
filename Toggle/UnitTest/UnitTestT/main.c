1de5f6e07695649d5f17a6560a239f66978d3a9d
vinlred <78894721+vinlred@users.noreply.github.com>
2022-10-04 13:47:14 +0700

    Add files via upload

---
+++ Toggle/UnitTest/UnitTestT/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "C:\Users\user\Desktop\Scanned\PSE\FSM_LED\Toggle\fsmtoggle\fsmtoggle.h"
+#include "C:\Users\user\Desktop\Scanned\PSE\FSM_LED\Toggle\fsmtoggle\fsmtoggle.c"
+
+int main(){
+    int state = STATE_OFF;
+
+}
